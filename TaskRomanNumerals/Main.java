3cc597c339a925204a4438f7bc64e76334b454d3
Данил <infografika@inbox.ru>
2023-02-11 20:16:46 +0600

    task Roman numerals

--- TaskRomanNumerals/Main.java
+++ TaskRomanNumerals/Main.java
@@ -1,13 +1,26 @@
 package TaskRomanNumerals;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        romanToInt("ABDSCSF");
     }
 
-    public int romanToInt(String s) {
+    public static int romanToInt(String s) {
         int result = 0;
 
+        Scanner scannerMyString = new Scanner(s);
+
+        HashMap<String, Integer> myDict = new HashMap<>();
+
+        String rimNamber = scannerMyString.nextLine();
+        String[] rimNamber2 = rimNamber.split("");
+        for (String el: rimNamber2) {
+            System.out.println(el);
+        }
 
 
         return result;
